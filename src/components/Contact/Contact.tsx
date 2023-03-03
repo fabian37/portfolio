4551f3a311fda10ca06690c5a67bc2e3d7bd759c
Fabian Gomez <fabian37gomez@gmail.com>
2023-03-03 15:55:50 -0500

    feat: add fonts and navigation

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/Contact.scss';
+export interface ContactProps {}
+
+const Contact: React.FC<ContactProps> = () => {
+	return (
+		<section className="contact-container" id="contact">
+			Contact
+		</section>
+	);
+};
+
+export default Contact;
